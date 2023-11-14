21be51e1e55843e89300e4ac06792fc65e36d1e6
ariefrwknd <arief.rahman@weekendinc.com>
2023-11-14 13:14:10 +0700

    add: target

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Target</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/",	
+	// base: "/",	
 	plugins: [
 		react(),
 		VitePWA({
@@ -44,6 +44,9 @@ export default defineConfig({
 		}),
 	],
 	build: {
+    build: {
+      target: "es2020",
+    },
 		commonjsOptions: {
 			transformMixedEsModules: true,
 		},
