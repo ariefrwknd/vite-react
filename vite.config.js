db418dd876dd63e20ae505e73435d487528a7971
ariefrwknd <121962953+ariefrwknd@users.noreply.github.com>
2023-11-16 10:52:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,80 +5,57 @@ import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-			registerType: "autoUpdate",
-			strategies: "injectManifest",
-			srcDir: "src",
-			filename: "sw.js",
-			// selfDestroying: true,
-			manifest: {
-				name: "Ayo Qoncierge",
-				short_name: "qoncierge",
-				description: "Ayo Qoncierge Application",
-				theme_color: "#ffffff",
-				start_url: "/",
-				display: "standalone",
-				background_color: "#ffffff",
-				icons: [
-					{
-						src: "/favicon-16x16.png",
-						sizes: "16x16",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-					{
-						src: "/favicon-32x32.png",
-						sizes: "32x32",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-					{
-						src: "/favicon-144x144.png",
-						sizes: "144x144",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-					{
-						src: "/favicon-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-					{
-						src: "/favicon-256x256.png",
-						sizes: "256x256",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-					{
-						src: "/favicon-384x384.png",
-						sizes: "384x384",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-					{
-						src: "/favicon-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-				push: {
-					icon: "/logo.png",
-				},
-			},
-		}),
-		legacy({
-			targets: ["ie >= 11"],
-      			additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-      			polyfills: ["es.array.iterator"],
-		}),
-	],
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true,
-		},
-	},
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
+      // selfDestroying: true,
+      manifest: {
+        name: "Ayo Qoncierge",
+        short_name: "qoncierge",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/x-icon",
+          },
+          {
+            src: "/favicon-144x144.png",
+            sizes: "144x144",
+            type: "image/x-icon",
+          },
+        ],
+        push: {
+          icon: "/logo.png",
+        },
+      },
+    }),
+    legacy({
+      // for ie11
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      polyfills: ["es.array.iterator"],
+    }),
+    // legacy({
+    // 	targets: [">0.3%, not dead", "not IE 11"],
+    // }),
+  ],
+  build: {
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
