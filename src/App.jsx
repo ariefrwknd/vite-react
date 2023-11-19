59097b27a90abe8e08663b4f08c9c1613ebb06f3
Arief Rahman Yusniardi <ayusniar@pmintl.net>
2023-11-20 05:52:19 +0700

    init routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import { RouterProvider } from "react-router-dom";
-import './App.css'
+import { router } from "./routes";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-			<RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
