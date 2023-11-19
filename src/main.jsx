9a8412be344b48315e14bae42be21e0b1f115e2d
Arief Rahman Yusniardi <ayusniar@pmintl.net>
2023-11-20 05:24:01 +0700

    add: PWA on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { registerSW } from "virtual:pwa-register";
 import App from './App.jsx'
 import './index.css'
+
+const intervalInDay = (60 * 60 * 1000) / (1000 * 60 * 60 * 24);
+
+registerSW({
+	onRegistered(r) {
+		r &&
+			setInterval(() => {
+				r.update();
+			}, intervalInDay);
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
