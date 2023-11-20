ad5cb13fffd21f8ded855e64df9677d532e2458d
Arief Rahman Yusniardi <ayusniar@pmintl.net>
2023-11-20 09:16:02 +0700

    condition service worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
 const intervalInDay = (60 * 60 * 1000) / (1000 * 60 * 60 * 24);
 
-registerSW({
-	onRegistered(r) {
-		r &&
-			setInterval(() => {
-				r.update();
-			}, intervalInDay);
-	},
-});
+if ("serviceWorker" in navigator) {
+  registerSW({
+    onRegistered(r) {
+      r &&
+        setInterval(() => {
+          r.update();
+        }, intervalInDay);
+    },
+  });
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
