146dfce291d400202f90e97aefb9928ee6c88c00
Arief Rahman Yusniardi <ayusniar@pmintl.net>
2023-11-20 10:08:57 +0700

    remove router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import { router } from "./routes";
 import "./App.css";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>legacy Target</h1>
+      <h1>legacy chunks: false</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+  // return <RouterProvider router={router} />;
 }
 
 export default App;
