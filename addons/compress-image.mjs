c584b50be5ce18111328c74e54dbb8719db288b7
Arief Rahman Yusniardi <ayusniar@pmintl.net>
2023-11-20 11:15:32 +0700

    add: adds

---
+++ addons/compress-image.mjs
@@ -0,0 +1,44 @@
+import { glob } from "glob"
+import imagemin from "imagemin"
+import mozJpeg from "imagemin-mozjpeg"
+import optiPNG from "imagemin-optipng"
+import gifSicle from 'imagemin-gifsicle'
+import svgo from "imagemin-svgo";
+
+const blob = "build_deploy/**/*.{jpg,JPG, jpeg, JPEG, png, svg, gif}"
+
+const compressImage = async (input, output) => {
+  const files = await imagemin([input], {
+    destination: output,
+    plugins: [
+      mozJpeg({
+        quality: 82,
+      }),
+      optiPNG({ optimizationLevel: 4 }),
+      svgo(),
+      gifSicle()
+    ]
+  })
+
+  files.map((file) => {
+    console.log(`✅ ${file.sourcePath} `);
+  });
+}
+
+const compressAllImages = () => {
+  console.log(`Compressing all images...`)
+  glob(blob, async function (er, files) {
+    for (const file of files) {
+      const input = file
+      const output = file.substring(0, file.lastIndexOf("/")) + "/";
+
+      try {
+        await compressImage(input, output)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+}
+
+compressAllImages()
---
+++ addons/convert-image.mjs
@@ -0,0 +1,33 @@
+import { glob } from "glob";
+import imagemin from "imagemin";
+import webp from "imagemin-webp";
+
+const blob = "build_deploy/**/*.{jpg,JPG,jpeg,JPEG,png}";
+
+const convertImage = async (input, output) => {
+  const files = await imagemin([input], {
+    destination: output,
+    plugins: [
+      webp({
+        quality: 80,
+      }),
+    ],
+  });
+
+  files.map((file) => {
+    console.log(`✅ ${file.destinationPath} `);
+  });
+};
+
+const convertAllImages = () => {
+  console.log(`Converting all images...`);
+  glob(blob, function (er, files) {
+    files.map((file) => {
+      const input = file;
+      const output = file.substring(0, file.lastIndexOf("/")) + "/";
+      convertImage(input, output);
+    });
+  });
+};
+
+convertAllImages();
